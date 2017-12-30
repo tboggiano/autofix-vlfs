ea45150861cfa701831113d361feb0285767724f
Tracy Boggiano <tracy.boggiano@channeladvisorcom>
2017-12-30 13:56:09 -0500

    Add support for 2017 dmv dm_db_log_stats
    Add support for 2017 dmv sys.dm_db_log_stats.  Just for efficiency.

--- 02_VLF_UpdateInfo.sql
+++ 02_VLF_UpdateInfo.sql
@@ -19,15 +19,11 @@ CREATE TABLE #VLFSummary
     )
  
 -- Need to accomodate SQL Server 2012 (version 11.0)
-DECLARE @versionString            VARCHAR(20),
-        @serverVersion            DECIMAL(10,5),
-        @sqlServer2012Version    DECIMAL(10,5)
+DECLARE @MajorVersion            VARCHAR(20)
  
-SET        @versionString    = CAST(SERVERPROPERTY('productversion') AS VARCHAR(20))
-SET        @serverVersion = CAST(LEFT(@versionString,CHARINDEX('.', @versionString)) AS DECIMAL(10,5))
-SET        @sqlServer2012Version = 11.0 -- SQL Server 2012
+SET        @MajorVersion    = CAST(SERVERPROPERTY('ProductMajorVersion') AS tinyint)
  
-IF(@serverVersion >= @sqlServer2012Version)
+IF(@MajorVersion >= 11)
     BEGIN
         -- Use the new version of the table  
         CREATE TABLE #VLFInfo2012
@@ -57,70 +53,83 @@ ELSE
             )
  
     END
- 
-DECLARE csr CURSOR FAST_FORWARD READ_ONLY
-FOR
-SELECT name
-FROM master.sys.databases WHERE database_id <> 2 
-AND [state] = 0
-AND is_read_only = 0 
- 
-OPEN csr
- 
-FETCH NEXT FROM csr INTO @DBName
- 
-WHILE (@@fetch_status <> -1)
+
+IF(@MajorVersion >= 14)
 BEGIN
-	SET @DBCCQuery = REPLACE(REPLACE(
-		'DBCC loginfo ("{{DatabaseName}}") WITH NO_INFOMSGS, TABLERESULTS'
-		,'"','''')
-		,'{{DatabaseName}}', @DBName)
-IF(@serverVersion >= @sqlServer2012Version)
-	BEGIN
-	TRUNCATE TABLE #VLFInfo2012
-		
-	INSERT INTO #VLFInfo2012
-		
-
-	EXEC (@DBCCQuery)
- 
-	SET @vlfcount = @@rowcount
- 
-	SELECT @activevlfcount = COUNT(*) 
-	FROM #VLFInfo2012 WHERE [Status] = 2
-	END
-	ELSE
-	BEGIN
-	TRUNCATE TABLE #VLFInfo2008
-		
-	INSERT INTO #VLFInfo2008
-		
-
-	EXEC (@DBCCQuery)
- 
-	SET @vlfcount = @@rowcount
- 
-	SELECT @activevlfcount = COUNT(*) 
-	FROM #VLFInfo2008 WHERE [Status] = 2
-	END
-	SELECT @currentlogsizeMB = (size/128) 
-	FROM master.sys.master_files 
-	WHERE type_desc = 'log' 
-		AND DB_NAME(database_id)=@DBName
- 
 	INSERT INTO #VLFSummary
-	VALUES(@DBName, @vlfcount, @activevlfcount, CONVERT(VARCHAR(7),DATABASEPROPERTYEX(@DBName, 'Recovery')), @currentlogsizeMB)
+	SELECT name, dls.total_vlf_count, dls.active_vlf_count, d.recovery_model_desc, dls.total_log_size_mb
+	FROM sys.databases d
+		CROSS APPLY sys.dm_db_log_stats(d.database_id) dls
+END
+ELSE 
+BEGIN
+	DECLARE csr CURSOR FAST_FORWARD READ_ONLY
+	FOR
+	SELECT name
+	FROM master.sys.databases 
+	WHERE database_id <> 2 
+		AND [state] = 0
+		AND is_read_only = 0 
+ 
+	OPEN csr
  
 	FETCH NEXT FROM csr INTO @DBName
+ 
+	WHILE (@@fetch_status <> -1)
+	BEGIN
+		SET @DBCCQuery = REPLACE(REPLACE(
+			'DBCC loginfo ("{{DatabaseName}}") WITH NO_INFOMSGS, TABLERESULTS'
+			,'"','''')
+			,'{{DatabaseName}}', @DBName)
+
+		IF(@MajorVersion >= 11)
+		BEGIN
+			TRUNCATE TABLE #VLFInfo2012
+		
+			INSERT INTO #VLFInfo2012
+			EXEC (@DBCCQuery)
+ 
+			SET @vlfcount = @@rowcount
+ 
+			SELECT @activevlfcount = COUNT(*) 
+			FROM #VLFInfo2012 
+			WHERE [Status] = 2
+		END
+		ELSE
+		BEGIN
+			TRUNCATE TABLE #VLFInfo2008
+		
+			INSERT INTO #VLFInfo2008
+			EXEC (@DBCCQuery)
+ 
+			SET @vlfcount = @@rowcount
+ 
+			SELECT @activevlfcount = COUNT(*) 
+			FROM #VLFInfo2008 WHERE [Status] = 2
+		END
+
+		SELECT @currentlogsizeMB = (size/128) 
+		FROM master.sys.master_files 
+		WHERE type_desc = 'log' 
+			AND DB_NAME(database_id)=@DBName
+ 
+		INSERT INTO #VLFSummary
+		VALUES(@DBName, @vlfcount, @activevlfcount, CONVERT(VARCHAR(7),DATABASEPROPERTYEX(@DBName, 'Recovery')), @currentlogsizeMB)
+ 
+		FETCH NEXT FROM csr INTO @DBName
+	END
+ 
+	CLOSE csr
+	DEALLOCATE csr
 END
- 
-CLOSE csr
-DEALLOCATE csr
- 
+
 TRUNCATE TABLE dbo.VLFInfo
- 
+
 INSERT INTO dbo.VLFInfo (DBName, NumOfVLFs, ActiveVLFs, RecoveryMode, LogSizeMB)
 SELECT DBName, NumOfVLFs, ActiveVLFs, RecoveryMode, LogSizeMB 
 FROM #VLFSummary
  
 DROP TABLE #VLFSummary  
+GO
+
+
--- README.md
+++ README.md
@@ -4,3 +4,5 @@ For more information how this works see:
 http://databasesuperhero.com/archive/2017/09/high-vlf-count-fix/
 
 After creating the objects create a SQL Agent job.  Step one will call procedure VLF_UpdateInfo.  Step two will call VLF_AutoFix with the parameter @LogBackJobName parameter specifying your log back job so it can help mark the VLFs a the end the log file not active.
+
+Added loaded VLFInfo table from new DMV sys.dm_db_log_stats when running on SQL Server 2017.
